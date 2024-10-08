78025f88c59fec7eec743b865d80fe9103594eb1
Darkelf7147 <c0b2304108@edu.teu.ac.jp>
2024-10-08 14:52:06 +0900

    2. implement new effect

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -167,6 +167,9 @@ def main():
             if bomb is not None:
                 if beam.rct.colliderect(bomb.rct):
                     beam, bomb = None, None
+                    bird.change_img(6, screen)
+                    pg.display.update()
+                    time.sleep(1)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
